ff4d1351a1d1f9ba873ac50366007aaa0481ab37
nicborg <nicolasborg@gmail.com>
2019-01-23 17:51:38 -0600

    Changing position and going back to ✕

--- src/ios/PhotoViewer.m
+++ src/ios/PhotoViewer.m
@@ -221,10 +221,10 @@
 
     if(showCloseBtn) {
         closeBtn = [UIButton buttonWithType:UIButtonTypeSystem];
-        [closeBtn setTitle:@"Close" forState:UIControlStateNormal];
+        [closeBtn setTitle:@"✕" forState:UIControlStateNormal];
         closeBtn.titleLabel.font = [UIFont systemFontOfSize: 32];
         [closeBtn setTitleColor:[UIColor colorWithRed:255/255.0 green:255/255.0 blue:255/255.0 alpha:0.6] forState:UIControlStateNormal];
-        [closeBtn setFrame:CGRectMake(0, viewHeight - 50, 50, 50)];
+        [closeBtn setFrame:CGRectMake(viewWidth - 50, 50, 50, 50)];
         [closeBtn setBackgroundColor:[UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.0]];
         [closeBtn addTarget:self action:@selector(closeButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
         [self.viewController.view addSubview:closeBtn];
